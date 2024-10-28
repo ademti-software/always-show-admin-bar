f7a29f4cae14a9dc7edaff923deb78bd01c8bb11
Lee Willis <lee@ademti-software.co.uk>
2024-10-28 14:22:48 +0000

    Copyright

--- always-show-admin-bar.php
+++ always-show-admin-bar.php
@@ -1,12 +1,35 @@
 <?php
 /*
-Plugin Name: Always Show Admin Bar
-Plugin URI: http://www.leewillis.co.uk/wordpress-plugins/
-Description: Always show the admin bar
-Author: Lee Willis
-Version: 0.3
-Author URI: http://www.leewillis.co.uk/
-*/
+ * Plugin Name: Always Show Admin Bar
+ * Plugin URI: http://www.ademti-software.co.uk/
+ * Description: Always show the admin bar
+ * Author: Ademti Software
+ * Version: 0.3
+ * Author URI: http://www.ademti-software.co.uk/
+ */
+
+/**
+ * Copyright (c) 2011-2015 Lee Willis. All rights reserved.
+ * Copyright (c) 2015-2024 Ademti Software. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
 
 class asabfa {
 
